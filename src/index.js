4be04b01160fc4350f028cfae612cc7352ca2ee4
AleksinaDidenko <aleksina.didenko@gmail.com>
2020-04-25 15:07:54 +0300

    drob

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
-  $(".owl-carousel").owlCarousel();
-
-  $(".owl-carousel").owlCarousel({
-    center:true,
-    loop: true,
-    nav: true,
-    autoWidth: true,
-    items: 1,
-  });
+  
 });
