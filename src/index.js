28be84ca97e33c9bd527a876fb1a5724179e2744
AleksinaDidenko <aleksina.didenko@gmail.com>
2020-04-25 16:48:23 +0300

    form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  
+  //   $(".multiple-items").slick({
+  //     infinite: true,
+  //     dots: true,
+  //     slidesToShow: 2,
+  //     slidesToScroll: 1,
+  //   });
+//   $(".single-item").slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     // autoplay: true,
+//     // autoplaySpeed: 4000,
+//   });
+
+
 });
