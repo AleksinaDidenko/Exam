f96e4789a2e854b7ef9ba2d9f357e924589c9305
AleksinaDidenko <aleksina.didenko@gmail.com>
2020-04-25 14:52:35 +0300

    ok

---
+++ src/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".owl-carousel").owlCarousel();
+
+  $(".owl-carousel").owlCarousel({
+    center:true,
+    loop: true,
+    nav: true,
+    autoWidth: true,
+    items: 1,
+  });
+});
