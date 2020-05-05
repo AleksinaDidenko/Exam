8a31bd2854c878a1120143da2e73c59315e978ba
AleksinaDidenko <aleksina.didenko@gmail.com>
2020-05-05 20:03:47 +0300

    slider swiper

--- src/index.js
+++ src/index.js
@@ -1,60 +1,41 @@
 $(document).ready(function() {
-  //   $(".multiple-items").slick({
-  //     infinite: true,
-  //     dots: true,
-  //     slidesToShow: 2,
-  //     slidesToScroll: 1,
-  //   });
-  //   $(".single-item").slick({
-  //     slidesToShow: 1,
-  //     slidesToScroll: 1,
-  //     // autoplay: true,
-  //     // autoplaySpeed: 4000,
-  //   });
-
-  // let nameVal = $("#commentForm #Name").val();
-  // $("#commentForm #Name").keyup(function(){
-  //   $("p").text(nameVal);
-  // })
-
-  //1.получить значение текстовых инпутов
-  //2.Проверить количество символов в имени(не меньше 3)
-  //3.Почта(проверить правильность написания)
-  //4.При нажатии на кнопку - проверить все ли поля заполненны,если не все поля заполнены, то вернуться и подсветить не заполненное, если все заполненны, то открыть модальное окно(див, который был display:hide,стал display: flex), в котором будет написано, что ваше сообщение успешно отправленно(в течении 5 секунд перезагрузить страницу).
-
-  //Проверка на правильное заполнение
-
-  $("#name").on("change", function() {
-    let $count = $("#name")
-      .val()
-      .length();
-    let $minCount = 3;
-
-    if ($count < $minCount) {
-      $(".name__input p")
-        .removeClass("help")
-        .addClass("show");
-    }
+  var swiper = new Swiper(".swiper-container", {
+    cssMode: true,
+    // spaceBetween: 135, //расстояние между слайдами, чтобі не накладівались друг на друга
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
   });
 
-  //Создаём функцию для обработки события "submit" в форме
+  // //1.получить значение текстовых инпутов
+  // //2.Проверить количество символов в имени(не меньше 3)
+  // //3.Почта(проверить правильность написания)
+  // //4.При нажатии на кнопку - проверить все ли поля заполненны,если не все поля заполнены, то вернуться и подсветить не заполненное, если все заполненны, то открыть модальное окно(див, который был display:hide,стал display: flex), в котором будет написано, что ваше сообщение успешно отправленно(в течении 5 секунд перезагрузить страницу).
 
-  $("#commentForm").on("submit", function() {
-    //1.получить значение текстовых инпутов
-    let $name = $("#name").val();
-    let $email = $("#email").val();
-    let $website = $("#website").val();
-    let $message = $("#comment").val();
+  // //Создаём функцию для обработки события "submit" в форме
 
-    //вывод данных в модальное окно
-    $("#forName").text($name);
-    $("#forEmail").text($email);
-    $("#forWebsite").text($website);
-    $("#forComment").text($message);
+  // $("#commentForm").on("submit", function() {
+  //   //1.получить значение текстовых инпутов
+  //   let $name = $("#name").val();
+  //   let $email = $("#email").val();
+  //   let $website = $("#website").val();
+  //   let $message = $("#comment").val();
 
-    //Вызов модального окна
-    $("div.modal__form")
-      .removeClass("no_active")
-      .addClass("active");
-  });
+  //   //вывод данных в модальное окно
+  //   $("#forName").text($name);
+  //   $("#forEmail").text($email);
+  //   $("#forWebsite").text($website);
+  //   $("#forComment").text($message);
+
+  //   //Вызов модального окна
+  //   $("div.modal__form")
+  //     .removeClass("no_active")
+  //     .addClass("active");
+  // });
 });
