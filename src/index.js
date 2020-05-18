c9db7783ce8e648f0d5d1d5237ae4e5f09475418
AleksinaDidenko <aleksina.didenko@gmail.com>
2020-05-18 22:09:55 +0300

    header_media

--- src/index.js
+++ src/index.js
@@ -12,30 +12,12 @@ $(document).ready(function() {
     mousewheel: true,
     keyboard: true,
   });
+ 
+  //search at home.html
+  $('#search').on('click', function(){
+       $('.header__nav label').toggleClass("active")
+   })
 
-  // //1.получить значение текстовых инпутов
-  // //2.Проверить количество символов в имени(не меньше 3)
-  // //3.Почта(проверить правильность написания)
-  // //4.При нажатии на кнопку - проверить все ли поля заполненны,если не все поля заполнены, то вернуться и подсветить не заполненное, если все заполненны, то открыть модальное окно(див, который был display:hide,стал display: flex), в котором будет написано, что ваше сообщение успешно отправленно(в течении 5 секунд перезагрузить страницу).
+   
 
-  // //Создаём функцию для обработки события "submit" в форме
-
-  // $("#commentForm").on("submit", function() {
-  //   //1.получить значение текстовых инпутов
-  //   let $name = $("#name").val();
-  //   let $email = $("#email").val();
-  //   let $website = $("#website").val();
-  //   let $message = $("#comment").val();
-
-  //   //вывод данных в модальное окно
-  //   $("#forName").text($name);
-  //   $("#forEmail").text($email);
-  //   $("#forWebsite").text($website);
-  //   $("#forComment").text($message);
-
-  //   //Вызов модального окна
-  //   $("div.modal__form")
-  //     .removeClass("no_active")
-  //     .addClass("active");
-  // });
 });
